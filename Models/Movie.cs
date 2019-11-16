aafe635de0b2eb90a5bec774cadbeb07c1ac0073
Morris-Trevon <tlove21@gmail.com>
2019-11-16 09:34:19 -0500

    Minor fix

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -21,7 +21,6 @@ namespace MvcMovie.Models
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
         public string Rating { get; set; }
     }
 }
